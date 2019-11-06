090e6ee0d9fab376fe4e583229e79e8d2e2610fd
Ryan Leach <rwmleach@gmail.com>
2019-11-05 19:49:41 -0500

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Ryan Leach on 11/5/19.
+//  Copyright © 2019 Ryan Leach. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
